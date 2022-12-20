d1329f25db48d1916e8d9a9109ad151355b74e94
Nicholas-Vandyck <nicholas.vandyck@amalitech.com>
2022-12-20 14:54:34 +0000

    working on JS

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const billType = document.querySelector(".bill-type");
-const peopleInput = document.querySelector(".people-input");
-const tipsPerPerson = document.getElementById("tip-amount");
-const totalPerPerson = document.getElementById("tip-amount1");
-const tips = document.querySelectorAll(".tips");
-const tipCustom = document.querySelector(".tip-custom");
-const resetBtn = document.querySelector(".reset");
-const error = document.querySelector(".error");
+var billType = document.querySelector(".bill-type");
+var peopleInput = document.querySelector(".people-input");
+var tipsPerPerson = document.getElementById("tip-amount");
+var totalPerPerson = document.getElementById("total-amount");
+var tips = document.querySelectorAll(".tips");
+var tipCustom = document.querySelector(".tip-custom");
+var resetBtn = document.querySelector(".reset");
+var error = document.querySelector(".error");
 
 billType.addEventListener("input", billType);
 peopleInput.addEventListener("input", peopleInput);
@@ -15,10 +15,11 @@ tips.forEach(function(val) {
 tipCustom.addEventListener("input", tipCustom);
 resetBtn.addEventListener("click", resetBtn);
 
-billType.value = "0.0";
-peopleInput.value = "1";
-totalPerPerson.innerHTML = "$" + (0.0).toFixed(2);
+billType.value = "0";
+peopleInput.value = "0";
 tipsPerPerson.innerHTML = "$" + (0.0).toFixed(2);
+totalPerPerson.innerHTML = "$" + (0.0).toFixed(2);
+
 
 let billValue = 0.0;
 let peopleValue = 1;
@@ -31,8 +32,6 @@ function billtype() {
 
 function peopleinput() {
     peopleValue = parseFloat(peopleInput.Value);
-   
-
     if(peopleValue < 1) {
         error.style.display = 'flex'
         peopleInput.style.border = 'thick solid red '
@@ -65,17 +64,17 @@ function handleClick(event){
 
 function calculateTip(){
     if(peopleValue >= 1){
-    let tipamount = (billValue * tipsPerPerson) /peopleValue;
-    let total = (billValue * tipamount) / peopleValue;
+    let tipamount = (billValue * tipsValue) /peopleValue;
+    let total = (billValue + tipamount) / peopleValue;
     tipsPerPerson.innerHTML = "$" + tipamount.toFixed(2);
     totalPerPerson.innerHTML = "$" + total.toFixed(2)
     }
 }
 
 function reset(){
-    billType.value = "0.0";
+    billType.value = "0";
     billType()
-    peopleInput.value = "1";
+    peopleInput.value = "0";
     peopleInput()
     tipCustom.value = ""
 }
